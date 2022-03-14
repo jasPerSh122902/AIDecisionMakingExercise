00d967c16990b3d9eee217a8e042f579e31d54a6
Miles <miles.gr.lee@gmail.com>
2022-03-14 09:37:52 -0500

    Agent1 Seeks ball then goal

--- raygame/Agent1.cpp
+++ raygame/Agent1.cpp
@@ -2,9 +2,11 @@
 #include "SpriteComponent.h"
 #include "Goal.h"
 #include "GameManager.h"
+#include "SeekMLComponent.h"
 
-Agent1::Agent1(float x, float y, const char* name, float maxForce, float maxSpeed, float health) : Character(x, y, name, maxForce, maxSpeed, health)
+Agent1::Agent1(float x, float y, const char* name, float maxForce, float maxSpeed, float health, Actor* ball) : Character(x, y, name, maxForce, maxSpeed, health)
 {
+	m_ball = ball;
 	addComponent(new SpriteComponent("Images/player.png"));
 }
 
@@ -16,13 +18,22 @@ void Agent1::onCollision(Actor* actor)
 void Agent1::start()
 {
 	Character::start();
-	//add steering behaviours here
+
+	GameManager* manager = GameManager::getInstance();
+	m_goal = manager->getRightGoal();
+
+	m_seekComponent = new SeekMLComponent();
+	m_seekComponent->setSteeringForce(100);
+	m_seekComponent->setTarget(m_ball);
+	addComponent(m_seekComponent);
 }
 
 void Agent1::update(float deltaTime)
 {
 	Character::update(deltaTime);
 
+	if (getHasBall())
+		m_seekComponent->setTarget(m_goal);
 }
 
 void Agent1::onDeath()
--- raygame/MainScene.cpp
+++ raygame/MainScene.cpp
@@ -11,16 +11,17 @@
 #include "GameManager.h"
 #include "ScoreBoard.h"
 #include "Engine.h"
+#include "SeekMLComponent.h"
 
 void MainScene::start()
 {
-	Agent1* agent1 = new Agent1(250, 400, "Agent1", 50, 50, 3);
-	Agent2* agent2 = new Agent2(Engine::getScreenWidth() - 250.0f, 400, "Agent2", 50, 50, 3);
-
 	Actor* ball = new Actor(Engine::getScreenWidth() / 2.0f, Engine::getScreenHeight() / 2.0f, "Ball");
 	ball->addComponent(new SpriteComponent("Images/ball.png"));
 	ball->getTransform()->setScale({100, 100 });
 	ball->setCollider(new CircleCollider(1, ball));
+
+	Agent1* agent1 = new Agent1(250, 400, "Agent1", 50, 50, 3, ball);
+	Agent2* agent2 = new Agent2(Engine::getScreenWidth() - 250.0f, 400, "Agent2", 50, 50, 3);
 
 	GameManager::getInstance()->init(agent1, agent2, ball, 3);
 
